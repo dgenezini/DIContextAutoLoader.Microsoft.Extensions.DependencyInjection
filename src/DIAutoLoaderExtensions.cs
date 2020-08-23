f041239f85c4b13370d3b67ce9f21d1a2c60f889
Daniel Genezini <danielg@grupoht.com.br>
2020-08-23 16:17:51 -0300

    Initial version

---
+++ src/DIAutoLoaderExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace DIContextAutoLoader.Microsoft.Extensions.DependencyInjection
+{
+    public static class DIAutoLoaderExtensions
+    {
+        public static IServiceCollection AutoLoadServices(
+            this IServiceCollection services,
+            params Assembly[] assemblies)
+        {
+            var InstanceInjectionConfigurarions = ServiceInjectionManager
+                .GetServicesInjectionConfigurarions(assemblies);
+
+            foreach (var InstanceInjectionConfigurarion in InstanceInjectionConfigurarions)
+            {
+                if (InstanceInjectionConfigurarion.Lifetime == InjectionLifetime.Transient)
+                {
+                    services.AddTransient(InstanceInjectionConfigurarion.ServiceType,
+                        InstanceInjectionConfigurarion.ImplementationType);
+                }
+                else if (InstanceInjectionConfigurarion.Lifetime == InjectionLifetime.Scoped)
+                {
+                    services.AddScoped(InstanceInjectionConfigurarion.ServiceType,
+                        InstanceInjectionConfigurarion.ImplementationType);
+                }
+                else if (InstanceInjectionConfigurarion.Lifetime == InjectionLifetime.Singleton)
+                {
+                    services.AddSingleton(InstanceInjectionConfigurarion.ServiceType,
+                        InstanceInjectionConfigurarion.ImplementationType);
+                }
+            }
+
+            return services;
+        }
+    }
+}
